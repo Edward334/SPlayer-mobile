f66d3c3e94f7611fde801269b336b94073383b0c
asterveil <mouwanzz114514@gmail.com>
2026-08-15 15:06:04 +0800

    Align Android media notification API

--- android/app/src/main/java/com/imsyy/splayer/SPlayerAudioService.java
+++ android/app/src/main/java/com/imsyy/splayer/SPlayerAudioService.java
@@ -13,7 +13,6 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
-import androidx.media.app.NotificationCompat.MediaStyle;
 
 public class SPlayerAudioService extends Service {
     private static final String CHANNEL_ID = "splayer-playback";
@@ -57,7 +56,6 @@ public class SPlayerAudioService extends Service {
                 .setSmallIcon(android.R.drawable.ic_media_play)
                 .setOngoing(true)
                 .setCategory(NotificationCompat.CATEGORY_TRANSPORT)
-                .setStyle(new MediaStyle().setMediaSession(mediaSession.getSessionToken()))
                 .build();
         startForeground(NOTIFICATION_ID, notification);
     }
